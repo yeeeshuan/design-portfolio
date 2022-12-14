bf2e101d392031a683f508b7a060f4a247940bec
Ethan Huang <yehuang2020@gmail.com>
2022-12-13 21:55:11 -0500

    updated interactive grid project

--- components/projects.js
+++ components/projects.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import styles from "../styles/Projects.module.css"; 
 import Image from "next/image";
 import P1 from '../Images/About/P1.png'; 
+import P2 from '../Images/About/P2.png'; 
 import P3 from '../Images/About/P3.png'; 
 import P4 from '../Images/About/P4.png'; 
 import P5 from '../Images/About/P5.jpg'; 
@@ -49,8 +50,7 @@ class Projects extends Component{
         super(props);
         this.state = {
             projects:[
-                {type : "v", name : "Teachable Grid", description: "Designing with ML", link: "/Teachable", 
-                thumbnail: "https://www.youtube.com/embed/7lnMo1m1O3g?autoplay=1&loop=1&mute=1&controls=0&playlist=7lnMo1m1O3g" }, 
+                {type : "i", name : "Interactive Grid", description: "Designing with ML", image: P2, link: "/Teachable"}, 
                 {type : "v", name : "Virtual Tour", description: "Prototyping with AR", link: "/Tour", 
                 thumbnail: "https://www.youtube.com/embed/topF8G5Y5ZY?start=98&end=104&autoplay=1&loop=1&mute=1&controls=0&playlist=topF8G5Y5ZY" }, 
                 {type : "i", name : "Math2Shine Redesign", description: "Interaction Design, Front-End Development", image : P1, link: "/Math2Shine" }, 
--- pages/Teachable.js
+++ pages/Teachable.js
@@ -3,12 +3,12 @@ import styles from "../styles/Projects.module.css";
 import Image from "next/image";
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
-import Thumbnail from "../Images/Projects/Teachable/Thumbnail.gif"
-import GTM from "../Images/Projects/Teachable/GTM.png"
+import Thumbnail from "../Images/About/P2.png"
+import GTM from "../Images/Projects/Teachable/GTM.png"; 
+import Figma from "..//Images/Projects/Teachable/Figma.png"
 
 export default function Teachable(){
     return(
-
         <div>
             <Navbar/>
             <div className = {styles.intro}>
@@ -19,17 +19,20 @@ export default function Teachable(){
             <p className = {styles.d1}>Role //</p><p className = {styles.d2}> Creative Coding, Interaction Design </p> 
             <br></br>
             <br></br>
-            <p className = {styles.d1}>Duration //</p><p className = {styles.d2}> July 2022 </p>
+            <p className = {styles.d1}>Duration //</p><p className = {styles.d2}> November 2022 </p>
             <br></br>
             <br></br>
-            <p className = {styles.d1}>Tools //</p><p className = {styles.d2}> HTML, P5.js, Google Teachable Machine, Web Speech API</p>
+            <p className = {styles.d1}>Tools //</p><p className = {styles.d2}> React.js, P5.js, Google Teachable Machine, ML5.js</p>
             </div>
             <div className = {styles.cont}> 
             <p className = {styles.text}>
             This project is a collection of interaction design experiments presented through the form of a game. Using the Google Teachable Machine 
-            and the Web Speech API, I was able to gain a deeper understanding of how to incorporate interactions into my designs—cases where they are helpful 
-            vs. cases where they become convoluted and confusing. 
+            and the ML5.js libraries, I was able to gain a deeper understanding of how to incorporate interactions into designs—if there is a better method to design 
+            interactions for the Human-Processing-Model. 
             </p>
+            <br/>
+            <a className = {styles.links} href = "https://yeeeshuan.github.io/interactive_grid/"> Live Website </a> 
+            <a className = {styles.links} href = "https://github.com/yeeeshuan/interactive_grid">Github</a>
          </div>
          </div>
             <div style = {{
@@ -39,6 +42,7 @@ export default function Teachable(){
             }}>
                 <Image src = {Thumbnail} width = {1080} height ={600} alt = "cover image"/>
             </div>
+            <br/>
             <div className = {styles.page}>
                 <div>
                 </div>
@@ -47,11 +51,13 @@ export default function Teachable(){
                     Problem Space 
                 </h1>
                     <p className = {styles.text}>
-                        Most technology today is limited in the number of ways one can navigate through it—typing on a keyboard or mouse, or tapping on a screen. 
-                        With this project, I wanted to move away from typing and tapping and experiment with other forms of interactions by prompting users to 
-                        move around and color a 10x10 grid. I explored with a variety of methods to: 
+                    The purpose of this website is to educate designers on machine learning libraries and methods to design for accessible interactions
+                    —a keyboard is often hard to use for those with disabilities that affect mobility, and hardware designed for accessibility is often 
+                    expensive and uncomfortable. The information I am conveying on this website is a variety of experimental interactions that users can 
+                    activate using their hands, faces, random objects around them, and sound. Users are encouraged to experiment with how to use the interactions 
+                    to: 
                         <br></br>
-                        <br></br>1. Determine a color for the grid
+                        <br></br>1. Set a color to a module. 
                         <br></br>
                         <br></br>2. Move across the grid. 
                     </p>
@@ -75,20 +81,47 @@ export default function Teachable(){
                     <div>
                     </div>
                     <div>
+                    <h1 className = {styles.pTitle}> 
+                    Color Assignment 
+                     </h1>
                     <h1 className = {styles.bTitle}> 
-                    Google Teachable Machine Visual Detection
+                    Visual Detection
                     </h1>
                     <p className = {styles.text}>
-                    When the user moves to a block, they have the option of coloring the block blue, purple, or pink using different hand gestures. 
-                    I trained the Google Teachable Machine to be able to detect three different hand gestures and connect them with different colors they would represent. 
+                    When the user moves to a block, they have the option of coloring the block blue, purple, or pink using different hand gestures.
+                    I used the ML5.js handpose library to be able to detect the number of fingers being held up and connect them with different colors 
+                     they would represent.
                     </p>    
                     </div>
                 </div>
                 <div className = {styles.page}>
                     <div>
-                    <p className = {styles.d}> 
-                   Training Google Teachable Machine to recognize different hand gestures
-                    </p>
+                        <p className = {styles.d}> 
+                        Model in action 
+                        </p>
+                    </div>
+                    <div>
+                    <iframe width="100%" height="500" src="https://www.youtube.com/embed/7TSUz23Ogxs?autoplay=1&loop=1&mute=1&controls=0&playlist=7TSUz23Ogxs" title="Handpose Demonstration" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    </div>
+                </div>
+                <div className = {styles.page}>
+                    <div>
+                    </div>
+                    <div>
+                        <h1 className = {styles.bTitle}> 
+                        Audio Detection
+                        </h1>
+                        <p className = {styles.text}>
+                        I used the Google Teachable Machine to incorporate auditory interactions in my website, training the models to be able to detect knocks, 
+                        crinkling of paper, and claps. 
+                        </p>    
+                    </div>
+                </div>
+                <div className = {styles.page}>
+                    <div>
+                        <p className = {styles.d}> 
+                        Training Google Teachable Machine to recognize sounds
+                        </p>
                     </div>
                     <div>
                    <Image width = "1920px" height = "1080px" src = {GTM} alt = "gtm"></Image>
@@ -96,27 +129,67 @@ export default function Teachable(){
                 </div>
                 <div className = {styles.page}>
                     <div>
-                    <p className = {styles.d}> 
-                    Model in action 
-                    </p>
+                        <p className = {styles.d}> 
+                        Model in action 
+                        </p>
                     </div>
                     <div>
-                    <iframe width = "100%" height = "500px"src="https://www.youtube.com/embed/ocN-7d36Ujc?autoplay=1&loop=1&mute=1&controls=0&playlist=ocN-7d36Ujc"
-                 frameBorder = "0" modestbranding = "1"></iframe>
-
+                    <iframe width = "100%" height = "500px"src="https://www.youtube.com/embed/myro03md-mc?autoplay=1&loop=1&mute=1&controls=0&playlist=myro03md-mc"
+                    frameBorder = "0" modestbranding = "1"></iframe>
                     </div>
                 </div>
                 <div className = {styles.page}>
                     <div>
                     </div>
                     <div>
+                        <h1 className = {styles.bTitle}> 
+                        Object Recognition
+                        </h1>
+                        <p className = {styles.text}>
+                        Thinking about different ways users can use a webcam to assign interactions, I decided to have the ML5.js objectDetection library 
+                        detect different items that users may have near them to assign colors to modules on the grid. 
+                        </p>
+                    </div>
+                </div>
+                <div className = {styles.page}>
+                    <div>
+                        <p className = {styles.d}> 
+                        Demonstration of how object detection interaction functions. First assign objects to color, then use objects to color modules. 
+                        </p>
+                    </div>
+                    <div>
+                        <p className = {styles.text}>
+                            Users show different items for the objectDetection library to detect different objects and allocate them to assigning colors and deleting colors. 
+                        </p>
+                    <iframe width = "100%" height = "500px" src="https://www.youtube.com/embed/O1qUn1EHfTQ?autoplay=1&loop=1&mute=1&controls=0&playlist=O1qUn1EHfTQ"
+                    frameBorder = "0" modestbranding = "1"></iframe>
+                    </div>
+                </div>
+                <div className = {styles.page}>
+                    <div>
+                    </div>
+                    <div>
+                    <h1 className = {styles.pTitle}> 
+                    Movement
+                     </h1>
                     <h1 className = {styles.bTitle}> 
                     Speech Recognition 
                     </h1>
                     <p className = {styles.text}>
-                    With the help of the Web Speech API, when the user communicates whether they want to go up, down, left, or right, the interface detects the 
-                    sound and moves on the grid accordingly. Due to the Web Speech API&rsquo;s long run times, using audio takes a longer time to move around the grid than typing on a keyboard would. 
+                    With the help of the Ml5.js library, when the user communicates whether they want to go up, down, left, or right, the interface detects the 
+                    sound and moves on the grid accordingly. 
                     </p>
+                    </div>
+                </div>
+                <div className = {styles.page}>
+                    <div>
+                    <p className = {styles.d}> 
+                    Audio interaction in action 
+                    </p>
+                    </div>
+                    <div>
+                    <iframe width = "100%" height = "500px"src="https://www.youtube.com/embed/183t1zx_YNQ?autoplay=1&loop=1&mute=1&controls=0&playlist=183t1zx_YNQ"
+                    frameBorder = "0" modestbranding = "1"></iframe>
                     </div>
                 </div>
             </div>
@@ -129,7 +202,7 @@ export default function Teachable(){
                     </h1>
                     <p className = {styles.text}>
                     Thinking about different ways that you can go about controlling the directions, I wanted to think about other ways that 
-                    I came up with the idea where I would use my face as controller, using the ML5 faceMesh API I was able to track when my face was turning 
+                    I came up with the idea where I would use my face as controller, using the ML5.js faceMesh library I was able to track when my face was turning 
                     left, right, up, and down. 
                     </p>
                     </div>
@@ -154,7 +227,7 @@ export default function Teachable(){
                     </div>
                     <div>
                     <p className = {styles.text}>
-                        To detect when a user turned their head right, left, up, and down, I found the points in the API that corresponded to the left and right of the
+                        To detect when a user turned their head right, left, up, and opened their mouth to move down, I found the points in the Facemesh library that corresponded to the left and right of the
                         cheeks, the forehead, and the mouth and found the distance of those points to the nose—when the distance passes or goes under a certain value, the grid would move. 
                     </p>
                     <iframe width = "100%" height = "500px" src="https://www.youtube.com/embed/hzHO7XYRJdw?autoplay=1&loop=1&mute=1&controls=0&playlist=hzHO7XYRJdw"
@@ -163,32 +236,32 @@ export default function Teachable(){
                     </div>
                 </div>
                 <div className = {styles.page}>
-                    <div>
-                    <p className = {styles.d}> 
-                    Demonstration of opening of mouth being used as a camera button
+                <div>
+                </div>
+                <div>
+                <h1 className = {styles.pTitle}> 
+                    Interface Design 
+                </h1>
+                     <p className = {styles.text}>
+                        Since many of the modes to interact with the interface of experimental, a big part of designing the interface was aimed at 
+                        educaating users on the purpose of the interactions and the process of onboarding users.  
                     </p>
-                    </div>
-                    <div>
+                    <br/>
                     <p className = {styles.text}>
-                       To add a function that could detect when to assign a spot on the grid a color without using the keyboard, I decided for the interface to assign a color 
-                       whenever the user opened their mouth. The mouse in a way almost acts like a camera button. 
+                       With the addition of a home page, individual descriptions to describe the interaction and libraries used, and pop-ups to 
+                       onboard users, users will understand the purposes of each interaction and how it can be used to design for accessibility. 
                     </p>
-                    <iframe width = "100%" height = "500px"src="https://player.vimeo.com/video/743183207?autoplay=1&loop=1&mute=1&controls=0"
-                 frameBorder = "0" modestbranding = "1"></iframe>
-
-                    </div>
+                    
+                </div>
                 </div>
                 <div className = {styles.page}>
                     <div>
+                    <p className = {styles.d}> 
+                   Original Figma mockups 
+                    </p>
                     </div>
                     <div>
-                    <p className = {styles.text}>
-                      Overall, the interaction provided users with a way to navigate an interface relatively precisely, as long as the interface
-                      can find the distance between points on the user&rsquo;s face. This interaction 
-                      could be used in an environment where using a keyboard is not ideal, such as unhygenic public touchpads. 
-
-                    </p>
-
+                   <Image src = {Figma} alt = "gtm"></Image>
                     </div>
                 </div>
                 <div className = {styles.page}>
@@ -200,14 +273,12 @@ export default function Teachable(){
                     </h1>
                     <p className = {styles.text}>
                     These different experiments allowed me to understand how I could implement different interactions into an interface, and how
-                    they could make my interaction design both more intuitive or convoluted. Some of these errors may stem from issues with the API, and 
-                    discomfort when interacting with the project may come from the habits that users have developed when using technology. 
+                    they could make my interaction design both more intuitive and usable for the user. The process of onboarding was especially hard to design for, since many of these interactions are experimental, 
+                    and users are not expected to know how to interact with the interface on first use. 
                     <br></br>
                     <br></br>
-                    Moving forward, I would like to move past relying on the camera to be the primary hardware that detects interactions and explore how
+                    Moving forward, I would like to move past relying on the camera and speaker to be the primary hardware that detects interactions and explore how
                     physical objects can be used in interaction design. 
-
-
                     </p>    
                     </div>
                 </div>
