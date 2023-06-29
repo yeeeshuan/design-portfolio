606d83c58fa79adef3666d9d3d2d5bb8e63f0618
Ethan Huang <yehuang2020@gmail.com>
2023-06-29 08:31:59 -0400

    added LD project folder

---
+++ pages/Landesberg/Websites.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'; 
+import styles from "../../styles/Projects.module.css"; 
+import Image from "next/image";
+import Head from 'next/head';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import TED from "../../Images/About/TEDxCMU.jpg"
+
+export default function TEDxCMU(){
+    return(
+        <div>
+            <Head>
+            <title>Ethan Huang Design | LD Websites</title>
+            </Head>
+            <Navbar/>
+            <main className={styles.main}>
+            <h1 className = {styles.mTitle}> 
+            Valparaiso Univeristy Magazine
+            </h1>
+            <div className = {styles.intro}>
+            <div>
+            <p className = {styles.d1}>Role //</p><p className = {styles.d2}> Interaction designer </p> 
+            <br></br>
+            <br></br>
+            <p className = {styles.d1}>Duration //</p><p className = {styles.d2}> June 2023 </p>
+            <br></br>
+            <br></br>
+            <p className = {styles.d1}>Tools //</p><p className = {styles.d2}> Figma</p>
+            </div>
+            <div className = {styles.cont}> 
+            <p className = {styles.text}>
+            Valparaiso University wanted their print magazine to move online. As a result, I designed a website that explored how magazine elements such as feature pages and sidebars could translate into a 
+            digital form. 
+            </p>
+         </div>
+         </div>
+            <div style = {{
+                display: "flex", 
+                justifyContent: "center", 
+            
+            }}>
+                <Image src = {TED} width = {1080} height ={600} alt = "cover image"/>
+            </div>
+        <div className={styles.space}>
+         <iframe width = "100%" height="600px" src="https://www.youtube.com/embed/PJDp8zVuNLw?h=a774012d6f&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            frameBorder = "0" modestBranding = "1"></iframe>
+         </div>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
