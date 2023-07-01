5de19e89a4449687fc008231b18c6b86651ee8b1
Ethan Huang <yehuang2020@gmail.com>
2023-07-01 19:09:44 -0400

    added more images to button

--- components/conf.js
+++ components/conf.js
@@ -3,6 +3,8 @@ import fav from "../public/Favicon.png";
 import One from "../Images/About/1.jpeg"; 
 import Two from "../Images/About/2.jpeg"; 
 import Three from "../Images/About/3.jpeg"; 
+import Four from "../Images/About/4.JPG"; 
+import Five from "../Images/About/5.jpeg"; 
 import Image from "next/image";
 import { Component } from "react"; 
 
@@ -11,7 +13,7 @@ class Confetti extends Component{
     constructor(){
             super();
             this.state={
-                rotation: [fav, One, Two, Three],
+                rotation: [fav, One, Two, Three, Four, Five],
                 index:0
             }
         }   
