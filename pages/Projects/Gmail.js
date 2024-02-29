0c8f40be45ba99aec71eddd981d38ac9078c2016
Ethan Huang <yehuang2020@gmail.com>
2024-02-29 10:32:09 -0500

    added new Gmail video

--- pages/Projects/Gmail.js
+++ pages/Projects/Gmail.js
@@ -257,7 +257,7 @@ export default function Gmail(){
                 </p>
             </div>
             <div>
-                <iframe width = "100%" height = "500px" src="https://www.youtube.com/embed/TrGwbS-Isfg?si=KDFSU968tIPSMvd1&amp;loop=1&amp;mute=1&amp;controls=1&amp;" frameBorder = "0" modestBranding = "1" allowFullScreen></iframe>
+                <iframe width = "100%" height = "500px" src="https://www.youtube.com/embed/sD3LYOQxr_w?si=qxx4Y6ETSSGwYUi9&amp;loop=1&amp;mute=1&amp;controls=1&amp;" frameBorder = "0" modestBranding = "1" allowFullScreen></iframe>
             </div>
         </div>
         <div className = {styles.page}>
