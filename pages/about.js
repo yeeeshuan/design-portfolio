2ed1f4304afdd7f73dacc56bd73514e7ce2ab008
nahtehuang <ehuang@zendaconsulting.com>
2025-01-31 09:11:35 -0500

    fixed about page, made mobile

--- pages/about.js
+++ pages/about.js
@@ -6,32 +6,8 @@ import Image from "next/image";
 import Head from 'next/head';
 import Footer from '../components/footer'; 
 import Me from '../Images/About/About_1.jpg'; 
-import Photo from '../Images/About/About_2.jpg'; 
-import Sketches from '../Images/About/About_3.jpg'; 
 
 const About = () => {
-
-    const [isHovering, setIsHovering] = useState(false);
-    const [twoIsHovering, twoSetIsHovering] = useState(false);
-
-    function enterPhoto(){
-        setIsHovering(true); 
-    }
-
-    function leavePhoto(){
-        setIsHovering(false); 
-    }
-
-    function enterSketch(){
-        console.log("sketch"); 
-        console.log(twoIsHovering); 
-        twoSetIsHovering(true); 
-        console.log(twoIsHovering); 
-    }
-
-    function leaveSketch(){
-        twoSetIsHovering(false); 
-    }
 
 return(
     <div className = {styles.page}>
@@ -40,26 +16,19 @@ return(
         </Head> 
         <div className = {invis.invisible}>
             <Image src = {Me} alt = "photo rotation"/>
-            <Image src = {Photo} alt = "photo rotation"/>
-            <Image src = {Sketches} alt = "photo rotation"/>
         </div>
         <Navbar/>
-        <div className = {styles.display}>
-        <div>
-            <span className = {styles.imageWrap_1}>
-                {isHovering? (<Image style = {{borderRadius: '10px', overflow: 'hidden'}} className = "Photo" src = {Photo}  alt = "Photo"/>)
-                : twoIsHovering? (<Image style = {{borderRadius: '10px', overflow: 'hidden'}} className = "About" src = {Sketches}  alt = "About"/>)
-                : <Image style = {{borderRadius: '10px', overflow: 'hidden'}} className = "Photo" src = {Me}  alt = "Photo"/>
-             
-}
- 
+        <div className={styles.imageWrap_1}>
+        <div className={styles.left}>
+            <span>
+                <Image style = {{borderRadius: '10px', overflow: 'hidden'}} className = "Photo" src = {Me} alt = "Photo"/>
             </span>
         </div> 
-        <div>
-        <h1 className = {styles.title}>
+        <div className={styles.right}>
+        <h1 className={styles.title}>
             Hey! My name is Ethan Huang. 
         </h1>
-        <p className = {styles.text}>
+        <p className={styles.text}>
 
             I am a senior at Carnegie Mellon studying <span className = {styles.CText}>Interaction Design</span> and <span className = {styles.CText}> Human-Computer-Interaction </span>
             with a minor in <span className = {styles.CText}>Computer Science</span>. I am interested in interaction design 
@@ -68,16 +37,7 @@ return(
             <br/>
             <br/>
 
-            When I am not designing, I can be seen taking 
-            <span className = {styles.CText}
-                onMouseEnter={enterPhoto}
-                onMouseLeave={leavePhoto}
-            > photos</span>,
-            
-            <span className = {styles.CText}
-                onMouseEnter={enterSketch}
-                onMouseLeave={leaveSketch}
-            > sketching</span>, or curating the perfect 
+            When I am not designing, I can be seen taking photos, sketching, or curating the perfect 
                 
             <span className = {styles.CText}>
                 <a href = "https://open.spotify.com/user/ethanhuangpizza?si=2ee86a0814ba4650"> Spotify playlist. 
