5dd681097e8b1e7f84cff7c235aa96620c32db64
nahtehuang <ehuang@zendaconsulting.com>
2025-02-04 18:54:25 -0500

    optimized videos

--- components/projects.js
+++ components/projects.js
@@ -127,13 +127,13 @@ class Projects extends Component{
         super(props);
         this.state = {
             projects:[
-                {type : "v", category:"P", name : "Siri AI", description: "Designing with GenAI, Frontend Development", thumbnail: "./Siri.mov", link: "Projects/Siri" }, 
+                {type : "v", category:"P", name : "Siri AI", description: "Designing with GenAI, Frontend Development", thumbnail: "./Siri.mp4", link: "Projects/Siri" }, 
                 {type : "v", category:"P", name : "Waymo x Airbnb", description: "Product Design", link: "Projects/Waymo", thumbnail: "./Waymo.mp4" },  
                 {type : "i", category:"P", name : "Zenda Internship", description: "Designing with GenAI, Frontend Development", image: Zenda, link: "Projects/Zenda" }, 
                 {type : "i", category:"P", name : "Replit Community Page", description: "Product Design, Frontend Development", image: Repl, link: "Projects/Replit"},
                 {type : "i", category:"P", name : "Gmail Filtering System Redesign", description: "Product Design, Frontend Development", image : Gmail, link: "Projects/Gmail" },
                 {type : "i", category:"P", name : "Landesberg Design Internship", description: "Web + Print Design", image: P7, link: "Projects/Landesberg"}, 
-                {type : "v", category:"C", name : "I Know a Place", description: "Product Design, Frontend Development", thumbnail: "./I_Know.mov", link: "Projects/I_Know" },  
+                {type : "v", category:"C", name : "I Know a Place", description: "Product Design, Frontend Development", thumbnail: "./I_Know.mp4", link: "Projects/I_Know" },  
                 {type : "i", category:"C", name : "Cookie Monster Cooking", description: "Designing with GenAI", image: Cookie, link: "Projects/Cookie"},
                 {type : "v", category:"C", name : "Lil' Garden", description: "XR Nurturing Experience", link: "Projects/Garden", thumbnail: "./Garden.mp4" },  
                 {type : "v", category:"C", name : "Interactive Grid", description: "Designing with ML", link: "Projects/Teachable", thumbnail: "./Teachable.mp4" }, 
