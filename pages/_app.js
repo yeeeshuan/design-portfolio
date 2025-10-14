5684a2c1be6c5c61855ce5a74112538c2b0532ae
yeeeshuan <yehuang2020@gmail.com>
2025-10-14 11:57:26 -0700

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
@@ -7,6 +8,21 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>My App</title>
+         {/* Google Analytics */}
+         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NZ9DNR5Z2J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KSFGGJQCND', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </Head>
       <Component {...pageProps} />
     </>
