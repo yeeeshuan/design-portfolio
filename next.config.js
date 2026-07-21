c079584929f574e74ab5fb9d64f6b92b22b2aeba
yeeeshuan <yehuang2020@gmail.com>
2026-07-21 12:40:15 -0700

    another one

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
 }
 
